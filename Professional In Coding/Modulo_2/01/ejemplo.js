09dbea5cd75a7fb536373bf4e61f56b45439099c
Díaz Bryan <marcos.mdahn@gmail.com>
2022-03-14 21:55:29 -0600

    Changes

--- Professional In Coding/Modulo_2/01/ejemplo.js
+++
@@ -1,2 +0,0 @@
-//Mediante el console se manda el metodo
-console.log("Que paso bandera ahora toca JS")
--- Professional In Coding/Modulo_2/02/ejemplo.js
+++
@@ -1,4 +0,0 @@
-//Para declarar variables se usa var
-var edad = 30
-var total = edad*2
-console.log(total)
